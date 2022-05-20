539fad079cbf1fbf8a4880a9258823313071db39
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-05-19 17:22:45 -0700

    refactored js script

--- JavaScript-Basics-Course1/small-projects/vacation-destination/script.js
+++ JavaScript-Basics-Course1/small-projects/vacation-destination/script.js
@@ -1,84 +1,90 @@
-var detailsForm = document.querySelector("#destination_details_form");
+(function () {
 
-detailsForm.addEventListener('submit', handleFormSubmit);
+    "use strict";
 
-function handleFormSubmit(event) {
-    event.preventDefault();
+    const detailsForm = document.querySelector("#destination_details_form");
 
-    //1. extract the value from each form field
-    var destName = event.target.elements['name'].value;
-    var destLocation = event.target.elements['location'].value;
-    var destPhoto = event.target.elements['photo'].value;
-    var destDesc = event.target.elements['description'].value;
+    detailsForm.addEventListener('submit', handleFormSubmit);
 
-    //2. clear out the form field
-    for (var i = 0; i < detailsForm.length; i++) {
-        detailsForm.elements[i].value = "";
+    function handleFormSubmit(event) {
+        event.preventDefault();
+
+        //1. extract the value from each form field
+        const destName = event.target.elements['name'].value;
+        const destLocation = event.target.elements['location'].value;
+        const destPhoto = event.target.elements['photo'].value;
+        const destDesc = event.target.elements['description'].value;
+
+        //2. clear out the form field
+        for (let i = 0; i < detailsForm.length; i++) {
+            detailsForm.elements[i].value = "";
+        }
+
+        //3. run a function that creates the new card
+        const destCard = createDestinationCard(destName, destLocation, destPhoto, destDesc);
+
+        //4. if needed, change the header at the top of the destination list
+        const wishListContainer = document.getElementById("destinations_container");
+
+        if (wishListContainer.children.length === 0) {
+            document.getElementById('title').innerHTML = "My Wish List";
+        }
+
+        //5. add the card
+        document.querySelector('#destinations_container').appendChild(destCard);
+    }
+    function createDestinationCard(name, location, photoUrl, description) {
+        const card = document.createElement('div');
+        card.className = "card";
+
+        const img = document.createElement('img');
+        img.setAttribute('alt', name);
+
+        const constantPhotoUrl = 'images/signpost.jpg';
+
+        if (photoUrl.length === 0) {
+            img.setAttribute('src', constantPhotoUrl);
+        } else {
+            img.setAttribute('src', photoUrl);
+        }
+
+        card.appendChild(img);
+
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        const cardTitle = document.createElement('h3');
+        cardTitle.innerText = name;
+        cardBody.appendChild(cardTitle);
+
+        const cardSubtitle = document.createElement('h4');
+        cardSubtitle.innerText = location;
+        cardBody.appendChild(cardSubtitle);
+
+        if (description.length !== 0) {
+            const cardText = document.createElement('p');
+            cardText.className = 'card-text';
+            cardText.innerText = description;
+            cardBody.appendChild(cardText);
+
+        }
+
+        const cardDeleteBtn = document.createElement('button');
+        cardDeleteBtn.innerText = "Remove";
+
+        cardDeleteBtn.addEventListener('click', removeDestination);
+        cardBody.appendChild(cardDeleteBtn);
+
+        card.appendChild(cardBody);
+
+        return card;
+
     }
 
-    //3. run a function that creates the new card
-    var destCard = createDestinationCard(destName, destLocation, destPhoto, destDesc);
-
-    //4. if needed, change the header at the top of the destination list
-    var wishListContainer = document.getElementById("destinations_container");
-
-    if (wishListContainer.children.length === 0) {
-        document.getElementById('title').innerHTML = "My Wish List";
+    function removeDestination(event) {
+        const card = event.target.parentElement.parentElement;
+        card.remove();
     }
 
-    //5. add the card
-    document.querySelector('#destinations_container').appendChild(destCard);
-}
-function createDestinationCard(name, location, photoUrl, description) {
-    var card = document.createElement('div');
-    card.className = "card";
 
-    var img = document.createElement('img');
-    img.setAttribute('alt', name);
-
-    var constantPhotoUrl = 'images/signpost.jpg';
-
-    if (photoUrl.length === 0) {
-        img.setAttribute('src', constantPhotoUrl);
-    } else {
-        img.setAttribute('src', photoUrl);
-    }
-
-    card.appendChild(img);
-
-    var cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-
-    var cardTitle = document.createElement('h3');
-    cardTitle.innerText = name;
-    cardBody.appendChild(cardTitle);
-
-    var cardSubtitle = document.createElement('h4');
-    cardSubtitle.innerText = location;
-    cardBody.appendChild(cardSubtitle);
-
-    if (description.length !== 0) {
-        var cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.innerText = description;
-        cardBody.appendChild(cardText);
-
-    }
-
-    var cardDeleteBtn = document.createElement('button');
-    cardDeleteBtn.innerText = "Remove";
-
-    cardDeleteBtn.addEventListener('click', removeDestination);
-    cardBody.appendChild(cardDeleteBtn);
-
-    card.appendChild(cardBody);
-
-    return card;
-
-}
-
-function removeDestination(event) {
-    var card = event.target.parentElement.parentElement;
-    card.remove();
-}
-
+}());
