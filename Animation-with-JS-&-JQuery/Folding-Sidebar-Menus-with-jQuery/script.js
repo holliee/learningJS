96988107480b4abb84f2e2be174fd7efc5dd9d94
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-05-20 11:58:18 -0700

    jQuery is much shorter than normal vanilla JS. Very short project

---
+++ Animation-with-JS-&-JQuery/Folding-Sidebar-Menus-with-jQuery/script.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+    //$('ul li ul').css("display", "none");
+    $('ul li ul').hide();
+    $('.menulink').click(function () {
+        const thisMenu = $(this).next('ul');
+        $('ul li ul').not(thisMenu).hide();
+        thisMenu.toggle();
+        /* 			if( thisMenu.is(':visible')){
+                        thisMenu.hide();
+                    } else {
+                        thisMenu.show();
+                    } */
+    });
+}());
