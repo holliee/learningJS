ed0ba95d41f7ec9fdadb57a93ea87c6650fb37af
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-05-24 09:10:49 -0700

    timer with slides

---
+++ Animation-with-JS-&-JQuery/Slider on a timer/script.js
@@ -0,0 +1,37 @@
+// Add your JS here
+$(window).on('load', function () {
+    "use strict";
+
+    const imageCount = $("#slider ul li").length;
+    const imageWidth = $("#slider ul li:first img").first().width();
+    const totalWidth = (imageCount * imageWidth) + 'px';
+
+    let leftPosition = 0;
+    let counter = 0;
+
+    $("#slider ul").css('width', totalWidth);
+
+    setInterval(function () {
+        counter++;
+        if (counter === imageCount) {
+            //handle cloning of <ul>
+            $("#slider ul").clone().appendTo("#slider");
+            $("#slider ul").last().css('left', imageWidth + 'px');
+
+            leftPosition = `-${totalWidth}`;
+
+            $('#slider ul').last().animate({ left: 0 }, 700, "easeInQuad");
+            $('#slider ul').first().animate({ left: leftPosition }, 700, "easeInQuad", function () {
+                $('#slider ul').first().remove();
+            });
+            counter = 0;
+
+        }
+        else {
+            leftPosition = `-${counter * imageWidth}px`;
+            $('#slider ul').animate({ left: leftPosition }, 700, "easeInQuad");
+        }
+
+    }, 3000);
+
+});
