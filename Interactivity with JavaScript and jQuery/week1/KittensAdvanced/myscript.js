4dfcbeaa4b27c156b9ad538d4bb6f21d09684143
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-05-24 15:54:45 -0700

    finished basic kitten flexslider

---
+++ Interactivity with JavaScript and jQuery/week1/KittensAdvanced/myscript.js
@@ -0,0 +1,11 @@
+$(window).on('load', function () {
+    $('.flexslider').flexslider(
+        {
+            animation: 'slide',
+            slideshowSpeed: 2000,
+            direction: 'vertical',
+            reverse: true,
+            pauseOnHover: true
+        }
+    );
+});
