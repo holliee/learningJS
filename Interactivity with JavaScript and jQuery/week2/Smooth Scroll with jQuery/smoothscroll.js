1032082270688fc0bca5d1ecd6f289b52bb42712
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-05-24 18:36:09 -0700

    added smooth scroll

--- Interactivity with JavaScript and jQuery/week2/Smooth Scroll with jQuery/smoothscroll.js
+++ Interactivity with JavaScript and jQuery/week2/Smooth Scroll with jQuery/smoothscroll.js
@@ -1,2 +1,15 @@
 // Smoothscroll Script...
+$('nav ul li a').click(function () {
+    var thisSection = $(this).attr('href');
+    var thisLink = $(this);
+
+    $('html, body').stop().animate({
+        scrollTop: $(thisSection).offset().top - 200
+    }, 800, "easeOutCirc", function () {
+        $('nav ul li a').removeAttr('class');
+        $(thisLink).addClass('selected');
+        // alert( $(window).scrollTop() );
+    });
+
+});
 
