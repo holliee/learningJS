7b7e594a3f2b6ab44fa00d8c7b7a11e14e1a4cfb
Hollie O'Brien <hobrien@seas.upenn.edu>
2022-05-25 11:54:26 -0700

    starting on scription project while finishing vanilla JS scroll

--- Interactivity with JavaScript and jQuery/week2/Smooth Scroll with JavaScript/script.js
+++ Interactivity with JavaScript and jQuery/week2/Smooth Scroll with JavaScript/script.js
@@ -1,1 +1,15 @@
-// JavaScript goes here
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach( function(eachLink){
+    eachLink.addEventListener('click', smoothScroll );
+} );
+
+function smoothScroll(event){
+    //do smooth scrolling
+    event.preventDefault();
+
+    const targetID = event.target.getAttribute('href');
+    const targetSection = document.querySelector(targetID);
+    const originalTop = Math.floor(targetSection.getBoundingClientRect().top ) - 200;
+    window.scrollBy( { top: originalTop, left: 0, behavior: 'smooth'} );
+}
